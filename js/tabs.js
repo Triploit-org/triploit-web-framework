7efe94827750c8f7ee81c3500730e9ecce495f91
NetLamp <oplpublishergerman@gmail.com>
2017-05-19 18:11:14 +0200

    Going back to original tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -1,26 +1,13 @@
 function Tab(name, content, redirect){
-    function iframeFix(contentElement){
-        var iframe = document.createElement("iframe");
-        iframe.setAttribute("src", content);
-        contentElement.appendChild(iframe);
-    }
-    
     function use($, contentElement){
         if(redirect){
             window.location = content;
         }else{
-            while(contentElement.children.length > 0){
-                contentElement.children[0].remove();
-            }
-            iframeFix(contentElement);
-            $.ajax({
+            contentElement.innerHTML = $.ajax({
                 url: content,
                 type: "GET",
                 success: function(data){
                     contentElement.innerHTML = data;
-                    if(contentElement.children.length == 0){
-                        iframeFix(contentElement);
-                    }
                 }
             });
         }
